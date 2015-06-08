41416282359db6f6c027dcef154fa45e4d08abe1
董明理 <418005608@qq.com>
2015-06-08 17:55:43 +0800

    image2atom

--- trajectory/util/image2atom.js
+++ trajectory/util/image2atom.js
@@ -6,7 +6,10 @@ define(function(require,exports,module){
 		random = require('./random');
 
 	function createConfigDialog(){
-
+		var tpl = '<div style="">
+						<div style="">图片名称:<input type="text" class="" /><div>
+						<div style="">atom个数:<input type="text" class="" /><div>
+					</div>';
 	}
 	
 	var canvas = cpi.createCanvasFullScreen();
