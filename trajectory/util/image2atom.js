b3bdbf4bb84e8daa28c31b3bea2c323661913577
董明理 <418005608@qq.com>
2015-06-09 18:26:28 +0800

    atom util update

--- trajectory/util/image2atom.js
+++ trajectory/util/image2atom.js
@@ -6,36 +6,11 @@ define(function(require,exports,module){
 		random = require('./random');
 
 	function createConfigDialog(){
-		function createItem(title){
-			var div = document.createElement('div'),
-				span = document.createElement('span'),
-				input = document.createElement('input');
-			div.style.cssText = "";
-			span.innerText = title;
-			input.style.cssText = "";
-			div.appendChild(span);
-			div.appendChild(input);
-
-			return div;
-		}
-		var tpl = '<div style="width: 100%; left: 0px; top: 0px; height: 100%; position: fixed; z-index: 299; display: block; background-color: #333333; opacity: 0.5"></div>'
-					+'<div style="position: absolute; z-index:300">'
-						+'<div style="">图片路径:<input type="text" class="" /><div>'
-						+'<div style="">atom个数:<input type="text" class="" /><div>'
-					+'</div>';
-		var dg_w = 500,dg_h = 600,
-			mask = document.createElement('div'),
-			dialog = document.createElement('div');
-		mask.style.cssText = "width: 100%; left: 0px; top: 0px; height: 100%; position: fixed; z-index: 299; display: block; background-color: #333333; opacity: 0.5";
-		dialog.style.cssText = "position: absolute; z-index:300;width:500px;height:600px";
-		dialog.style.width = dg_w;
-		dialog.style.height = dg_h;
-		dialog.style.left = window.innerWidth/2 - dg_w/2;
-		dialog.style.top = window.innerHeight/2 - dg_h/2;
-		dialog.appendChild(createItem("图片路径："));
-
-
-		document.body.appendChild(mask);
+		var tpl = '<div style=""><span style="display:block"></span><input type="range" class="" /></div>'
+				+ '<div style=""><span style="display:block"></span><input type="checkbox" class="" /></div>';
+		var dialog = document.createElement('div');
+		dialog.style.cssText = "position: absolute; z-index:300; right:0; top:0; margin:50px; font-size:12px";
+		dialog.innerHTML = tpl;
 		document.body.appendChild(dialog);
 	}
 	
