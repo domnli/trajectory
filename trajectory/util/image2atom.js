f071fa618668aee846b39170384cd19eb645a2e9
董明理 <418005608@qq.com>
2015-06-12 19:08:17 +0800

    atomutil update

--- trajectory/util/image2atom.js
+++ trajectory/util/image2atom.js
@@ -3,17 +3,28 @@
 */
 define(function(require,exports,module){
 	var cpi = require('./cpi'),
-		random = require('./random');
+		random = require('./random'),
+		$ = require('jquery');
+		console.log($);
 
 	function createConfigDialog(){
-		var tpl = '<div style=""><span style="display:block">覆盖率</span><input type="range" class="" /></div>'
-				+ '<div style=""><span style="display:block">乱序</span><input type="checkbox" class="" /></div>';
-		var dialog = document.createElement('div');
-		dialog.style.cssText = "position: absolute; z-index:300; right:0; top:0; margin:50px; font-size:12px";
-		dialog.innerHTML = tpl;
-		document.body.appendChild(dialog);
+		var tpl = '<div style=""><span style="display:block">抽取率</span><input type="range" class="item extract" /></div>'
+				+ '<div style=""><span style="display:block">乱序</span><input type="checkbox" class="item disorder" /></div>';
+		var panl = document.createElement('div');
+		panl.id = "configPanl";
+		panl.style.cssText = "position: absolute; z-index:300; right:0; top:0; margin:50px; font-size:12px";
+		panl.innerHTML = tpl;
+		document.body.appendChild(panl);
+	}
+
+	function getConfig(){
+		var config = {};
+		config.extract = $('#configPanl .extract').val();
+		config.disorder = $('#configPanl .disorder').val();
+		return config;
 	}
 	
 	var canvas = cpi.createCanvasFullScreen();
 	createConfigDialog();
+	console.log(getConfig());
 });
