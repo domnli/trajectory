93c851a016e93281e0d3d3d0442e8e106fa37d8d
domnli <dml8880596@gmail.com>
2015-08-05 23:35:32 +0800

    circle

--- trajectory/circle.js
+++ trajectory/circle.js
@@ -1,13 +1,13 @@
 define(function(require,exports,module){
 	var Emitter = require("./util/emitter"),
-		Atom = require("./atom"),
-		queue = [];
+		Atom = require("./atom");
 
 	function Circle(conf){
-		this.atoms = [];
+		this.atoms = this.queue = [];
 		this.radius = conf.radius||10;
 		this.center = {x:conf.x||10,y:conf.y||10};
 		this.vel = conf.vel||0.001;
+		this.radian = conf.radian||0;
 	}
 
 	Circle.prototype.addAtom = function(atom){
@@ -17,12 +17,24 @@ define(function(require,exports,module){
 			}else{
 				this.atoms.push(new Atom({x:atom.x,y:atom.y}));
 			}
-			queue.push({this.center.x+this.radius,this.center.y});
+			this.queue.push({x:this.center.x+this.radius,y:this.center.y});
 		}
-		
 	}
 
 	Circle.prototype.next = function(){
-		
+		var i,temp,
+			queue = this.queue;
+		temp = queue[0];
+		if(temp == undefined){return;}
+		this.radian = this.radian + this.vel;
+		queue[0].x = Math.cos(this.radian) * this.radius + this.center.x;
+		queue[0].y = Math.sin(this.radian) * this.radius + this.center.y;
+		for(i=queue.length-1;i>1;i--){
+			queue[i] = queue[i-1];
+		}
+		for(i=0;i<this.atoms.length;i++){
+			this.atoms[i].set('x',queue[i].x);
+			this.atoms[i].set('y',queue[i].y);
+		}
 	}
 });
